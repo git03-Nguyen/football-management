8cc452b85987f68f9b03fe0e94a191a55fa6b001
21120171 <nguyendinhanhvlqt@gmail.com>
2024-01-07 09:40:54 +0700

    complete UI login

--- src/controllers/auth.c.js
+++ src/controllers/auth.c.js
@@ -17,17 +17,22 @@ module.exports = {
 
   // GET /register
   getRegister: function (req, res, next) {
-
+    res.send('GET /register');
   },
 
   // POST /register
   postRegister: function (req, res, next) {
-
+    res.send('POST /register');
   },
 
   // GET /logout
   getLogout: function (req, res, next) {
+    res.send('GET /logout');
+  },
 
+  // GET /forgot-password
+  getForgotPassword: function (req, res, next) {
+    res.send('GET /forgot-password');
   },
 
 
--- src/routers/general/auth.r.js
+++ src/routers/general/auth.r.js
@@ -6,6 +6,8 @@ const controller = require('../../controllers/auth.c');
 router.get('/login', controller.getLogin);
 router.post('/login', controller.postLogin);
 
+router.get('/forgot-password', controller.getForgotPassword);
+
 router.get('/register', controller.getRegister);
 router.post('/register', controller.postRegister);
 
