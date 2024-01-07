51f20a92b21bce2e1a53be4daaca7b181ad972d1
21120171 <nguyendinhanhvlqt@gmail.com>
2024-01-07 12:36:10 +0700

    add SSL certs, but still errors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routers/index.r')(app);
 
+const fs = require('fs');
+// read the ssl certificates from certificates/demo.crt and certificates/demo.key
+const privateKey = fs.readFileSync('certificates/demo.key', 'utf8');
+const certificate = fs.readFileSync('certificates/demo.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const https = require('https');
+const httpsServer = https.createServer(credentials, app);
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+httpsServer.listen(port, () => {
+  console.log(`Server is running on https://localhost:${port}`);
 });
