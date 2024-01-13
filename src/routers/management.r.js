a7c4e877a0f2dd074dbd5e002ea7ec2c6fe0d373
21120171 <nguyendinhanhvlqt@gmail.com>
2024-01-13 15:01:03 +0700

    Change avatar's href to /management

--- src/routers/management.r.js
+++ src/routers/management.r.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/management.c');
 
 const { checkAuthenticated, checkAdmin } = require('../utils/auth-helper');
 
-router.get('/', checkAuthenticated, controller.getTeamManagement);
+router.get('/', /*checkAuthenticated,*/ controller.getTeamManagement);
 
 
 module.exports = router;
