c93b809540ad7cd0e414401feb9db57d0a3e6d96
21120171 <nguyendinhanhvlqt@gmail.com>
2024-01-15 09:17:50 +0700

    Modify for main-branch

--- src/utils/database/db-config.js
+++ src/utils/database/db-config.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 
-// const pool = new Pool({
-//   host: process.env.DB_HOST || 'localhost',
-//   port: process.env.DB_PORT || 5432,
-//   user: process.env.DB_USER || 'postgres',
-//   password: process.env.DB_PASSWORD || 'postgres',
-//   database: process.env.DB_NAME || 'postgres',
-// });
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'postgres',
 });
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// });
 
 pool.connect((err) => {
   if (err) throw err
